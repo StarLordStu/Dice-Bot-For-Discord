c81faee59dad2c68382db763e2e4adb8463023dc
Stu <44758021+StarLordStu@users.noreply.github.com>
2021-10-25 20:32:54 -0400

    started Roll Function

--- roll.js
+++ roll.js
@@ -3,4 +3,9 @@ function rollDie(numSides) {
 }
 
 function roll(input) {
+    if (context.params.event.content.startsWith("/r")) {
+        let rolls = context.params.event.content.split(' ').slice(1);
+
+        let rollSUm = 0;
+    }
 }
