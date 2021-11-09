be7c0a3179f7bc0e9e6e1462a0281dbc0dc21e95
Stu <44758021+StarLordStu@users.noreply.github.com>
2021-11-09 13:15:34 -0500

    new function for rolling dice

--- roll.js
+++ roll.js
@@ -2,8 +2,8 @@ function rollDie(numRolls, numSides) {
     //this function is a for loop that will roll a (numSides) die (numRoll) times
     let result = 0
     for(let step = 0; step < numRolls; step++){
-        result = 1 + Math.floor(Math.random()*numSides)
+        result += (1 + Math.floor(Math.random()*numSides))
     }
+    console.log(result);
     return result
 }
-rollDie(1,6);
