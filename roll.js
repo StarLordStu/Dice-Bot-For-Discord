43e93d7a9484b0f650420a050a16033d78d83eba
Stu <44758021+StarLordStu@users.noreply.github.com>
2021-11-11 14:03:51 -0500

    added 2nd function to create variables based on given string

--- roll.js
+++ roll.js
@@ -1,3 +1,14 @@
+//this function takes the text from the input and puts it in a variable
+function formdata() 
+        {
+        let rollReq= document.getElementById("request").value;
+
+            let [numRolls, numSides, modifier] = rollReq.split(/[dD+-]{1}/);
+            //let mod = rollReq.match(/([+-]{1})([0-9]{1,})/);
+            console.log(numRolls, numSides, modifier);
+            rollDie(numRolls, numSides, modifier);
+        }    
+
 function rollDie(numRolls, numSides) {
     //this function is a for loop that will roll a (numSides) die (numRoll) times
     let result = 0
