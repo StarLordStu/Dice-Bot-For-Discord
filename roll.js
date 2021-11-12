2c8c5cfb3c1137f5c858ceb2d1ebc1c9c23a443f
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2021-11-11 22:54:29 -0500

    rework roll function

--- roll.js
+++ roll.js
@@ -1,31 +1,46 @@
 //this function takes the text from the input and puts it in a variable
-function formdata() 
-        {
-        let rollReq= document.getElementById("request").value;
+function formdata() {
+    let rollReq = document.getElementById("request").value;
 
-            let [numRolls, numSides, modifier] = rollReq.split(/[dD+-]{1}/);
-            //let mod = rollReq.match(/([+-]{1})([0-9]{1,})/);
-            console.log(numRolls, numSides, modifier);
-            rollDie(numRolls, numSides, modifier);
-        }    
+    let [numRolls, numSides, modifier] = rollReq.split(/[dD+-]{1}/);
+    //let mod = rollReq.match(/([+-]{1})([0-9]{1,})/);
+    console.log(numRolls, numSides, modifier);
+    rollDie(numRolls, numSides, modifier);
+}
 
 //this function is a for loop that will roll a (numSides) die (numRoll) times
 function rollDie(numRolls, numSides, modifier) {
-    
+
     //this if statement will hellp account for missing value for modifier. ex(2d4)
-    if (modifier == undefined){
+    if (modifier == undefined) {
         modifier = 0;
     }
-    ]
+
     if (numRolls && numSides)
         var result = 0
-        
-        for(let step = 0; step < numRolls; step++){
 
-            result += (1 + Math.floor(Math.random()*numSides))
-        }
-        //+ + will turn the variable from a string into a number
-        let total = result + + modifier
+    for (let step = 0; step < numRolls; step++) {
+
+        result += (1 + Math.floor(Math.random() * numSides))
+    }
+    //+ + will turn the variable from a string into a number
+    let total = result + + modifier
     console.log(total);
     return total
+}
+
+function roll(str) {
+    let [numRolls, numSides, modifier] = str.split(/[dD+-]{1}/);
+    let total = 0;
+    for (let i = 0; i < numRolls; i++) {
+        total += rollSingleDie(numSides);
+    }
+    if (modifier !== undefined) {
+        total += modifier;
+    }
+    return total;
+}
+
+function rollSingleDie(numSides) {
+    return 1 + Math.floor(Math.random() * numSides);
 }
